f06c9cb27fb4b14814e98ee1aa2155e7299009b1
Paymaun <paymaun.heidari@microsoft.com>
2018-05-19 10:52:09 -0700

    Use Az CLI edge build for CI.

--- scripts/ci/test_source.sh
+++ scripts/ci/test_source.sh
@@ -5,7 +5,9 @@ set -ex
 echo "Installing azure-cli-testsdk and azure-cli..."
 
 # Update the git commit or branch when we need a new version of azure-cli-testsdk
+pip install --pre azure-cli --extra-index-url https://azurecliprod.blob.core.windows.net/edge
 pip install "git+https://github.com/Azure/azure-cli@master#egg=azure-cli-testsdk&subdirectory=src/azure-cli-testsdk" -q
+
 echo "Installed."
 az --version
 
