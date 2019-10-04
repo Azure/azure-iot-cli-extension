4df623e10d50a58ded18ac1b97a1cbfda2e4feed
Paymaun Heidari <paymaun.heidari@microsoft.com>
2019-10-04 16:12:18 -0700

    Initial DevOps CI configuration.

--- scripts/ci/test_static_py2.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-proc_number=`python -c 'import multiprocessing; print(multiprocessing.cpu_count())'`
-
-# Run pylint/flake8 on IoT extension
-pylint azext_iot/ --ignore=events3 --rcfile=./.pylintrc -j $proc_number
-flake8 azext_iot/ --statistics --exclude=events3 --config=setup.cfg
