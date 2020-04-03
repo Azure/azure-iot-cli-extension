ecbd9d19ed48aff37b7eb90236f9dddd6c6d373a
prbans <prbans@microsoft.com>
2020-04-03 16:21:00 -0700

    version bump

--- azext_iot/constants.py
+++ azext_iot/constants.py
@@ -7,7 +7,7 @@
 
 import os
 
-VERSION = "255.9.2"
+VERSION = "255.9.3"
 EXTENSION_NAME = "azure-iot"
 EXTENSION_ROOT = os.path.dirname(os.path.abspath(__file__))
 EXTENSION_CONFIG_ROOT_KEY = "iotext"
