cbd39446756ff93caa775cabcb8c21f1fcb4603b
Paymaun Heidari <paymaun.heidari@microsoft.com>
2020-04-13 14:42:00 -0700

    Fix central int tests mvp.

--- azext_iot/tests/test_iot_central_int.py
+++ azext_iot/tests/test_iot_central_int.py
@@ -13,7 +13,7 @@ DEVICE_ID = os.environ.get("azext_iot_central_device_id")
 if not all([APP_ID, DEVICE_ID]):
     raise ValueError(
         "Set azext_iot_central_app_id "
-        "and azext_iot_central_device_id to run integration tests. "
+        "and azext_iot_central_device_id to run central integration tests. "
     )
 
 
@@ -24,14 +24,14 @@ class TestIotCentral(LiveScenarioTest):
     def test_central_device_show(self):
         # Verify incorrect app-id throws error
         self.cmd(
-            'az iotcentral device-twin show --app-id incorrect-app --device-id "{}"'.format(
+            'iotcentral device-twin show --app-id incorrect-app --device-id "{}"'.format(
                 DEVICE_ID
             ),
             expect_failure=True,
         )
         # Verify incorrect device-id throws error
         self.cmd(
-            'az iotcentral device-twin show --app-id "{}" --device-id incorrect-device'.format(
+            'iotcentral device-twin show --app-id "{}" --device-id incorrect-device'.format(
                 APP_ID
             ),
             expect_failure=True,
@@ -39,7 +39,7 @@ class TestIotCentral(LiveScenarioTest):
         # Verify that no errors are thrown when device shown
         # We cannot verify that the result is correct, as the Azure CLI for IoT Central does not support adding devices
         self.cmd(
-            'az iotcentral device-twin show --app-id "{}" --device-id "{}"'.format(
+            'iotcentral device-twin show --app-id "{}" --device-id "{}"'.format(
                 APP_ID, DEVICE_ID
             )
         )
@@ -52,14 +52,14 @@ class TestIotCentral(LiveScenarioTest):
         )
         # Ensure no failure
         # We cannot verify that the result is correct, as the Azure CLI for IoT Central does not support adding devices
-        self.cmd("iotcentral app monitor-events --app-id {}".format(APP_ID))
+        self.cmd("iotcentral app monitor-events --app-id {} --to 1".format(APP_ID))
 
     def test_central_validate_messages(self):
         # Test with invalid app-id
         self.cmd(
-            "iotcentral app validate-messages --app-id {}".format(APP_ID + "zzz"),
+            "iot central app validate-messages --app-id {}".format(APP_ID + "zzz"),
             expect_failure=True,
         )
         # Ensure no failure
         # We cannot verify that the result is correct, as the Azure CLI for IoT Central does not support adding devices
-        self.cmd("iotcentral app validate-messages --app-id {}".format(APP_ID))
+        self.cmd("iot central app validate-messages --app-id {} --to 1".format(APP_ID))
