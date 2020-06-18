a099acc912f84eb13431e625d36ad04b0f060a50
Paymaun Heidari <paymaun.heidari@microsoft.com>
2020-06-17 17:28:49 -0700

    Staging point.

--- azext_iot/iothub/providers/base.py
+++ azext_iot/iothub/providers/base.py
@@ -4,7 +4,7 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-from azext_iot.common._azure import get_iot_hub_connection_string
+from azext_iot.iothub.providers.discovery import IotHubDiscovery
 from azext_iot._factory import SdkResolver
 from msrest.exceptions import SerializationError
 from msrestazure.azure_exceptions import CloudError
@@ -18,10 +18,10 @@ class IoTHubProvider(object):
         self.cmd = cmd
         self.hub_name = hub_name
         self.rg = rg
-        self.target = get_iot_hub_connection_string(
-            cmd=self.cmd,
+        self.discovery = IotHubDiscovery(cmd)
+        self.target = self.discovery.get_target(
             hub_name=self.hub_name,
-            resource_group_name=self.rg,
+            rg=self.rg,
             login=login,
         )
         self.resolver = SdkResolver(self.target)
