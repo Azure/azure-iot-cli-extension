ed7ac0d753f65f4367e841b50e263c0f57061e15
Raj valluri <navallu@microsoft.com>
2020-06-24 01:56:25 -0700

    additional fixes

--- azext_iot/central/commands_device_twin.py
+++ azext_iot/central/commands_device_twin.py
@@ -4,12 +4,8 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-from knack.util import CLIError
+
 from azext_iot.constants import CENTRAL_ENDPOINT
-from azext_iot._factory import _bind_sdk
-from azext_iot.common.shared import SdkType
-from azext_iot.common.utility import unpack_msrest_error
-from azext_iot.common.sas_token_auth import BasicSasTokenAuthentication
 from azext_iot.central.providers.device_provider import get_device_twin
 
 
