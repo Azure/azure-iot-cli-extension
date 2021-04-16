f3767964ad86cadf21801c30fa986119f1f128cb
Ryan K <ryan.k@outlook.com>
2021-04-16 16:21:38 -0700

    Update jsonschema dependency and remove pkg_resources working set workaround (#328)

--- azext_iot/models/__init__.py
+++ azext_iot/models/__init__.py
@@ -3,8 +3,3 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-
-
-from azext_iot.common.utility import ensure_pkg_resources_entries
-
-ensure_pkg_resources_entries()
