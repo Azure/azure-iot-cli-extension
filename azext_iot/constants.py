0b22a46eb7b1c7fe48e97d19c235bcd9391a62cf
Paymaun <Paymaunh@microsoft.com>
2021-04-26 12:01:57 -0700

    Fix a couple issues for v0.10.11 (#333)
    * Tweak adt api interaction sleep time.

--- azext_iot/constants.py
+++ azext_iot/constants.py
@@ -7,7 +7,7 @@
 
 import os
 
-VERSION = "0.10.10"
+VERSION = "0.10.11"
 EXTENSION_NAME = "azure-iot"
 EXTENSION_ROOT = os.path.dirname(os.path.abspath(__file__))
 EXTENSION_CONFIG_ROOT_KEY = "iotext"
