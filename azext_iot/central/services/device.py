37db091877482177f4af865fc73b706af50756c5
George Trifonov <geotri@microsoft.com>
2021-04-29 14:23:55 -0700

    adding message about default ttl

--- azext_iot/central/services/device.py
+++ azext_iot/central/services/device.py
@@ -333,6 +333,9 @@ def run_manual_failover(
     json = {}
     if ttl_minutes :
         json = {"ttl": ttl_minutes}
+    else:
+        print("""Using default time to live - 
+        see https://github.com/iot-for-all/iot-central-high-availability-clients#readme for more information""")
 
     response = requests.post(url, headers=headers, verify=not should_disable_connection_verify(), json=json)
     _utility.log_response_debug(response=response, logger=logger)
