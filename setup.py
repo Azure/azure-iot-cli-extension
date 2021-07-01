d36d9c3f6f3259fc4953e349845713bfb0037321
Victoria Litvinova <vilit@microsoft.com>
2021-06-30 18:16:44 -0700

    add extras

--- setup.py
+++ setup.py
@@ -50,6 +50,10 @@ if not PACKAGE_NAME:
 
 DEPENDENCIES = ["paho-mqtt==1.5.0", "jsonschema==3.2.0", "packaging"]
 
+EXTRAS = {
+    "uamqp": ["uamqp~=1.2"]
+}
+
 
 CLASSIFIERS = [
     "Development Status :: 4 - Beta",
@@ -85,5 +89,6 @@ setup(
         ]
     },
     install_requires=DEPENDENCIES,
+    extras_require=EXTRAS,
     zip_safe=False
 )
