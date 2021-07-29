7b1c728f7598aebfc5ece455204dfd35dadd87a2
Avin Agrawal <avagraw@microsoft.com>
2021-07-29 02:43:11 -0700

    use service principal name for role assignment

--- azext_iot/tests/__init__.py
+++ azext_iot/tests/__init__.py
@@ -131,7 +131,7 @@ class IoTLiveScenarioTest(CaptureOutputLiveScenarioTest):
                 new_hub = self.cmd(
                     "iot hub show -n {} -g {}".format(self.entity_name, self.entity_rg)
                 ).get_output_in_json()
-                user_id = "00000000-0000-0000-0000-000000000000"
+                user_id = "avin_nightly_connection"
                 # assign IoT Hub Data Contributor role to current user
                 self.cmd(
                     '''role assignment create --assignee "{}" --role "{}" --scope "{}"'''.format(
