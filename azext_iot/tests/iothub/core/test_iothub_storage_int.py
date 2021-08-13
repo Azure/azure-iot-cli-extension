71420c6f7951b6a447afb02d8c5f9d5f6793b0ca
Avin Agrawal <avagraw@microsoft.com>
2021-08-12 21:41:53 -0700

    lint fixes

--- azext_iot/tests/iothub/core/test_iothub_storage_int.py
+++ azext_iot/tests/iothub/core/test_iothub_storage_int.py
@@ -125,7 +125,7 @@ class TestIoTStorage(IoTLiveScenarioTest):
                 role_assignments = self.get_role_assignments(LIVE_STORAGE_RESOURCE_ID, STORAGE_ROLE)
                 role_assignment_principal_ids = [assignment["principalId"] for assignment in role_assignments]
                 sleep(10)
-            
+
             sleep(30)
 
     def tearDown(self):
