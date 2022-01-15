a19bbeb849c363871c25d8eb69756f251dc579b9
Avin Agrawal <avagraw@microsoft.com>
2022-01-14 16:34:56 -0800

    version updated

--- azext_iot/constants.py
+++ azext_iot/constants.py
@@ -7,7 +7,7 @@
 
 import os
 
-VERSION = "255.253.1"
+VERSION = "255.253.2"
 EXTENSION_NAME = "azure-iot"
 EXTENSION_ROOT = os.path.dirname(os.path.abspath(__file__))
 EXTENSION_CONFIG_ROOT_KEY = "iotext"
