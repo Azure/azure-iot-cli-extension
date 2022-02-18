5b726116e2e0c38276b2cc0e9de74db57a771495
Victoria Litvinova <vilit@microsoft.com>
2022-02-17 16:21:36 -0800

    fix skip if

--- azext_iot/tests/digitaltwins/test_dt_data_history_lifecycle_int.py
+++ azext_iot/tests/digitaltwins/test_dt_data_history_lifecycle_int.py
@@ -26,7 +26,7 @@ ADH_REGION = "northeurope"
 enabled_data_history = os.environ.get("ADT_DH_PREVIEW_ENABLE")
 
 
-@pytest.mark.skipif(not enabled_data_history, "Wait until Data History Public Preview")
+@pytest.mark.skipif(not enabled_data_history, reason="Wait until Data History Public Preview")
 class TestDTConnections(DTLiveScenarioTest):
     def __init__(self, test_case):
         super(TestDTConnections, self).__init__(test_case)
