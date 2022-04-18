40b4c81a9e6fd861484a4c321c78fbdff2b3d147
Victoria Litvinova <vilit@microsoft.com>
2022-04-18 09:48:07 -0700

    reformat certops.py with black

--- azext_iot/common/certops.py
+++ azext_iot/common/certops.py
@@ -17,7 +17,9 @@ from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.primitives.asymmetric import rsa
 
 
-def create_self_signed_certificate(subject, valid_days, cert_output_dir, cert_only=False):
+def create_self_signed_certificate(
+    subject, valid_days, cert_output_dir, cert_only=False
+):
     """
     Function used to create a self-signed certificate
 
@@ -35,35 +37,36 @@ def create_self_signed_certificate(subject, valid_days, cert_output_dir, cert_on
     key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
 
     # create a self-signed cert
-    subject_name = x509.Name([
-        x509.NameAttribute(NameOID.COMMON_NAME, subject),
-    ])
-    cert = x509.CertificateBuilder().subject_name(
-        subject_name
-    ).issuer_name(
-        subject_name
-    ).public_key(
-        key.public_key()
-    ).serial_number(
-        x509.random_serial_number()
-    ).not_valid_before(
-        datetime.datetime.utcnow()
-    ).not_valid_after(
-        datetime.datetime.utcnow() + datetime.timedelta(days=valid_days)
-    ).sign(key, hashes.SHA256())
+    subject_name = x509.Name(
+        [
+            x509.NameAttribute(NameOID.COMMON_NAME, subject),
+        ]
+    )
+    cert = (
+        x509.CertificateBuilder()
+        .subject_name(subject_name)
+        .issuer_name(subject_name)
+        .public_key(key.public_key())
+        .serial_number(x509.random_serial_number())
+        .not_valid_before(datetime.datetime.utcnow())
+        .not_valid_after(
+            datetime.datetime.utcnow() + datetime.timedelta(days=valid_days)
+        )
+        .sign(key, hashes.SHA256())
+    )
 
     cert_dump = key.private_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PrivateFormat.TraditionalOpenSSL,
         encryption_algorithm=serialization.NoEncryption(),
-    ).decode('utf-8')
-    key_dump = cert.public_bytes(serialization.Encoding.PEM).decode('utf-8')
+    ).decode("utf-8")
+    key_dump = cert.public_bytes(serialization.Encoding.PEM).decode("utf-8")
     thumbprint_bytes = cert.fingerprint(hashes.SHA1())
-    thumbprint = ''.join(f'{b:02X}' for b in thumbprint_bytes)
+    thumbprint = "".join(f"{b:02X}" for b in thumbprint_bytes)
 
     if cert_output_dir and exists(cert_output_dir):
-        cert_file = subject + '-cert.pem'
-        key_file = subject + '-key.pem'
+        cert_file = subject + "-cert.pem"
+        key_file = subject + "-key.pem"
 
         with open(join(cert_output_dir, cert_file), "wt", encoding="utf-8") as f:
             f.write(cert_dump)
@@ -73,9 +76,9 @@ def create_self_signed_certificate(subject, valid_days, cert_output_dir, cert_on
                 f.write(key_dump)
 
     result = {
-        'certificate': cert_dump,
-        'privateKey': key_dump,
-        'thumbprint': thumbprint
+        "certificate": cert_dump,
+        "privateKey": key_dump,
+        "thumbprint": thumbprint,
     }
 
     return result
@@ -93,11 +96,13 @@ def open_certificate(certificate_path):
         certificate (str): returns utf-8 encoded value from certificate file.
     """
     certificate = ""
-    if certificate_path.endswith('.pem') or certificate_path.endswith('.cer'):
+    if certificate_path.endswith(".pem") or certificate_path.endswith(".cer"):
         with open(certificate_path, "rb") as cert_file:
             certificate = cert_file.read()
             try:
                 certificate = certificate.decode("utf-8")
             except UnicodeError:
                 certificate = base64.b64encode(certificate).decode("utf-8")
-    return certificate.rstrip()  # Remove trailing white space from the certificate content
+    return (
+        certificate.rstrip()
+    )  # Remove trailing white space from the certificate content
