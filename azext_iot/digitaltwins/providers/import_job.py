2caf0674c1708634a1b683dfbeb605467f0dea08
Avin Agrawal <avagraw@microsoft.com>
2022-05-31 11:14:43 -0700

    Refactoring the storage account url fetch process

--- azext_iot/digitaltwins/providers/import_job.py
+++ azext_iot/digitaltwins/providers/import_job.py
@@ -13,12 +13,34 @@ from knack.log import get_logger
 
 logger = get_logger(__name__)
 
-
 class ImportJobProvider(DigitalTwinsProvider):
     def __init__(self, cmd, name, rg=None):
         super(ImportJobProvider, self).__init__(cmd=cmd, name=name, rg=rg)
         self.sdk = self.get_sdk().import_jobs
         self.cli = EmbeddedCLI()
+    
+    def _get_blob_url(self, blob_name, blob_container, storage_account):
+        storage_account_cstring_op = self.cli.invoke(
+            "storage account show-connection-string -n '{}'".format(storage_account)
+        )
+        if not storage_account_cstring_op.success():
+            raise CLIInternalError(
+                "Unable to retrieve connection string for input storage account: {}".format(storage_account)
+            )
+        storage_account_cstring = storage_account_cstring_op.as_json()
+        blob_url_op = self.cli.invoke(
+            "storage blob url --connection-string '{}' --container-name '{}' --name '{}'".format(
+                storage_account_cstring, blob_container, blob_name
+            )
+        )
+        if not blob_url_op.success():
+            raise CLIInternalError(
+                "Unable to retrieve blob url for import data file: {} in {} container under {} account".format(
+                    blob_name, blob_container, storage_account
+                )
+            )
+        blob_url = blob_url_op.as_json()
+        return blob_url
 
     def get(self, job_id):
         try:
@@ -48,39 +70,8 @@ class ImportJobProvider(DigitalTwinsProvider):
         if output_blob_container is None:
             output_blob_container = input_blob_container
 
-        input_storage_account_cstring_op = self.cli.invoke(
-            "az storage account show-connection-string -n {}".format(input_storage_account)
-        )
-        if not input_storage_account_cstring_op.success():
-            raise CLIInternalError(
-                "Unable to retrieve connection string for input storage account: {}".format(input_storage_account)
-            )
-        input_storage_account_cstring = input_storage_account_cstring_op.as_json()
-        input_blob_url_op = self.cli.invoke(
-            "storage blob url --connection-string {} --container-name {} --name {}".format(
-                input_storage_account_cstring, input_blob_container, input_blob_name
-            )
-        )
-        if not input_blob_url_op.success():
-            raise CLIInternalError(
-                "Unable to retrieve blob url for import data file: {} in {} container under {} account".format(
-                    input_blob_name, input_blob_container, input_blob_container
-                )
-            )
-        input_blob_url = input_blob_url_op.as_json()
-
-        output_blob_url_op = self.cli.invoke(
-            "storage blob url --account-name {} --container-name {} --name {}".format(
-                output_storage_account, output_blob_container, output_blob_name
-            )
-        )
-        if not output_blob_url_op.success():
-            raise CLIInternalError(
-                "Unable to retrieve blob url for import data file: {} in {} container under {} account".format(
-                    output_blob_name, output_blob_container, output_blob_container
-                )
-            )
-        output_blob_url = output_blob_url_op.as_json()
+        input_blob_url = self._get_blob_url(input_blob_name, input_blob_container, input_storage_account)
+        output_blob_url = self._get_blob_url(output_blob_name, output_blob_container, output_storage_account)
 
         try:
             import_job = BulkImportJob(input_blob_uri=input_blob_url, output_blob_uri=output_blob_url)
