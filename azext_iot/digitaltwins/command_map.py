a147baf8e42f5b79a3c8fc9e15ec27a281b16c3b
Avin Agrawal <avagraw@microsoft.com>
2022-06-15 18:19:40 -0700

    Mark dt job import commands as private preview

--- azext_iot/digitaltwins/command_map.py
+++ azext_iot/digitaltwins/command_map.py
@@ -214,6 +214,7 @@ def load_digitaltwins_commands(self, _):
     with self.command_group(
         "dt job import",
         command_type=digitaltwins_job_ops,
+        is_preview=True,
     ) as cmd_group:
         cmd_group.command("create", "create_import_job")
         cmd_group.show_command("show", "show_import_job")
