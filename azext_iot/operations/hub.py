37f8caa3c7a03779134f17c9f48684bb927e5235
Mirabai Smoot <t-msmoot@microsoft.com>
2022-06-27 16:28:32 -0700

    added migrate functionality

--- azext_iot/operations/hub.py
+++ azext_iot/operations/hub.py
@@ -100,6 +100,7 @@ def iot_device_show(
     login=None,
     auth_type_dataplane=None,
 ):
+
     discovery = IotHubDiscovery(cmd)
     target = discovery.get_target(
         resource_name=hub_name,
