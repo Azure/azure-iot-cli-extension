209dff895b11342281d059e5deb48a048d3fe424
Victoria Litvinova <vilit@microsoft.com>
2022-07-15 13:47:34 -0700

    add experimental flag

--- azext_iot/iothub/command_map.py
+++ azext_iot/iothub/command_map.py
@@ -50,6 +50,8 @@ def load_iothub_commands(self, _):
         cmd_group.command("send", "iot_c2d_message_send")
         cmd_group.command("purge", "iot_c2d_message_purge")
 
-    with self.command_group("iot hub certificate root-authority", command_type=iothub_resource_ops) as cmd_group:
+    with self.command_group(
+        "iot hub certificate root-authority", command_type=iothub_resource_ops, is_experimental=True
+    ) as cmd_group:
         cmd_group.show_command("show", "certificate_root_authority_show")
         cmd_group.command("set", "certificate_root_authority_set")
