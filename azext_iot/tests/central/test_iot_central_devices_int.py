609fc147b08c7b1590a1b322bae4915786e5c794
Victoria Litvinova <vilit@microsoft.com>
2022-09-06 17:36:04 -0700

    update test

--- azext_iot/tests/central/test_iot_central_devices_int.py
+++ azext_iot/tests/central/test_iot_central_devices_int.py
@@ -346,8 +346,9 @@ class TestIotCentralDevices(CentralLiveScenarioTest):
             device_id=device_id,
             module_name='testModule',
             component_name='ModuleComponent')
-        assert update_device_properties['$metadata']['testFirstProperty']['desiredValue'] == 'updatedTestFirstProperty'
-        assert update_device_properties['$metadata']['testSecondProperty']['desiredValue'] == 'updatedTestSecondProperty'
+        metadata = update_device_component_properties['$metadata']
+        assert metadata['testComponentFirstProperty']['desiredValue'] == 'updatedTestComponentFirstProperty'
+        assert metadata['testComponentSecondProperty']['desiredValue'] == 'updatedTestComponentSecondProperty'
 
         # Show module component properties
         command = f'''
