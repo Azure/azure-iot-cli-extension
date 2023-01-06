2cb15b3daa92d2bc99b5f9f361e6a8e52ea716f7
Ryan <ryan.k@outlook.com>
2023-01-06 11:58:35 -0800

    Added encoding and license info to fileops.py

--- azext_iot/common/fileops.py
+++ azext_iot/common/fileops.py
@@ -1,3 +1,9 @@
+# coding=utf-8
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
 from os import makedirs, remove, listdir
 from os.path import exists, join
 from pathlib import PurePath
