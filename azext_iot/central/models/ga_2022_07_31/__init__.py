00a1f28054bc88faab40dd249d0ece81936d1039
Ryan K <ryan.k@outlook.com>
2023-01-26 09:22:02 -0800

    Added encoding and license info to all files (except SDK) and add unit test (#616)

--- azext_iot/central/models/ga_2022_07_31/__init__.py
+++ azext_iot/central/models/ga_2022_07_31/__init__.py
@@ -1,3 +1,9 @@
+# coding=utf-8
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
 from azext_iot.central.models.ga_2022_07_31.api_token import ApiToken as ApiTokenGa
 from azext_iot.central.models.ga_2022_07_31.device_group import DeviceGroup as DeviceGroupGa
 from azext_iot.central.models.ga_2022_07_31.device import Device as DeviceGa
