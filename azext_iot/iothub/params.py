798926ab73b6b1bfd84d371e3d0d212e589325cd
Elsie4ever <3467996@gmail.com>
2023-05-04 16:53:28 -0700

    fix period space

--- azext_iot/iothub/params.py
+++ azext_iot/iothub/params.py
@@ -299,7 +299,7 @@ def load_iothub_arguments(self, _):
         context.argument(
             "resource_group_name",
             options_list=["--destination-resource-group", "--dg"],
-            help="Name of resource group of the IoT Hub to which the origin hub state will be copied to."
+            help="Name of resource group of the IoT Hub to which the origin hub state will be copied to. "
             "If not provided, will use the origin IoT Hub's resource group.",
             arg_group="IoT Hub Identifier"
         )
