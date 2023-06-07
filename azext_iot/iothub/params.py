fb85b31dbd7648cf5b7f42cc044db2dc963eb6e3
Victoria Litvinova <vilit@microsoft.com>
2023-06-07 14:42:55 -0700

    fix param

--- azext_iot/iothub/params.py
+++ azext_iot/iothub/params.py
@@ -566,11 +566,7 @@ def load_iothub_arguments(self, _):
             "{YYYY}. Any one placeholder may be specified at most once, but order and non-placeholder components are "
             "arbitrary. If partition key name is provided, partition key template defaults to {deviceid}-{YYYY}-{MM}",
         )
-        context.argument(
-            "endpoint_account_name",
-            options_list=["--endpoint-account"],
-            help="The account name for the endpoint resource.",
-        )
+
     for endpoint_type in ["cosmosdb-container", "storage-container"]:
         for endpoint_op in ["create", "update"]:
             with self.argument_context(f"iot hub message-endpoint {endpoint_type} {endpoint_op}") as context:
