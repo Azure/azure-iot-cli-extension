a68c95bde4411d901b67d707ccf0fb5e1397b661
Victoria Litvinova <73560279+vilit1@users.noreply.github.com>
2023-06-30 14:35:37 -0700

    History fix (#673)

--- azext_iot/constants.py
+++ azext_iot/constants.py
@@ -7,7 +7,7 @@
 
 import os
 
-VERSION = "0.21.3"
+VERSION = "0.21.4"
 EXTENSION_NAME = "azure-iot"
 EXTENSION_ROOT = os.path.dirname(os.path.abspath(__file__))
 EXTENSION_CONFIG_ROOT_KEY = "iotext"
