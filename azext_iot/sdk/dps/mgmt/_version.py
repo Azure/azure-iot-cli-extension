3ede41c104499c4dd9931c965e5ed3341bcbf210
Ryan Kelly <rykelly@microsoft.com>
2025-08-25 17:18:20 -0700

    better autorest of DPS MGMT SDK

--- azext_iot/sdk/dps/mgmt/_version.py
+++ azext_iot/sdk/dps/mgmt/_version.py
@@ -6,18 +6,4 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-from ._operations import Operations
-from ._operations import IotDpsResourceOperations
-from ._operations import DpsCertificateOperations
-
-from ._patch import __all__ as _patch_all
-from ._patch import *  # pylint: disable=unused-wildcard-import
-from ._patch import patch_sdk as _patch_sdk
-
-__all__ = [
-    "Operations",
-    "IotDpsResourceOperations",
-    "DpsCertificateOperations",
-]
-__all__.extend([p for p in _patch_all if p not in __all__])
-_patch_sdk()
+VERSION = "0.1.0"
