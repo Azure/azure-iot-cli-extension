8a5a9db4f8a7abc0c28c8d5bf5b3ddfcdbf1dc4f
Ryan Kelly <rykelly@microsoft.com>
2025-09-10 15:23:04 -0700

    version bump for bash

--- azext_iot/constants.py
+++ azext_iot/constants.py
@@ -7,7 +7,7 @@
 
 import os
 
-VERSION = "0.0.30-dev1"
+VERSION = "0.0.30-dev2"
 EXTENSION_NAME = "azure-iot"
 EXTENSION_ROOT = os.path.dirname(os.path.abspath(__file__))
 EXTENSION_CONFIG_ROOT_KEY = "iotext"
