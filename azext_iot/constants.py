e14e56bacb383bd18881799e2d941ae98aa69082
Ryan Kelly <rykelly@microsoft.com>
2025-09-12 10:54:36 -0700

    private preview version bump

--- azext_iot/constants.py
+++ azext_iot/constants.py
@@ -7,7 +7,7 @@
 
 import os
 
-VERSION = "0.0.30-dev2"
+VERSION = "0.1.0a1"
 EXTENSION_NAME = "azure-iot"
 EXTENSION_ROOT = os.path.dirname(os.path.abspath(__file__))
 EXTENSION_CONFIG_ROOT_KEY = "iotext"
