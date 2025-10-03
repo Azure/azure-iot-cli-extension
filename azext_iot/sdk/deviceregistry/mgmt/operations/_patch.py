bf2765620947e57d12250fd3974035ab39178c23
Ryan Kelly <rykelly@microsoft.com>
2025-10-03 09:05:48 -0700

    ADR SDK MIT license

--- azext_iot/sdk/deviceregistry/mgmt/operations/_patch.py
+++ azext_iot/sdk/deviceregistry/mgmt/operations/_patch.py
@@ -1,7 +1,6 @@
 # ------------------------------------
 # Copyright (c) Microsoft Corporation.
-# Private distribution for preview customers
-# Governed by license terms at https://aka.ms/iothub-certmgmt-privprev-license
+# Licensed under the MIT License.
 # ------------------------------------
 """Customize generated code here.
 
