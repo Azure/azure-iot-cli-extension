2fa15020ccb1b68f95702f4eb95af1eb68ed33c8
Ryan Kelly <rykelly@microsoft.com>
2025-10-09 11:09:21 -0700

    chore: version bump to 0.30.0b1

--- azext_iot/constants.py
+++ azext_iot/constants.py
@@ -7,7 +7,7 @@
 
 import os
 
-VERSION = "0.28.0a1"
+VERSION = "0.30.0b1"
 EXTENSION_NAME = "azure-iot"
 EXTENSION_ROOT = os.path.dirname(os.path.abspath(__file__))
 EXTENSION_CONFIG_ROOT_KEY = "iotext"
