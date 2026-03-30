3ba59bea6538ff810663ce68debae0386d693cb5
Hangyi Wang <hangyiwang@microsoft.com>
2026-03-30 21:34:43 +0000

    verdion

--- azext_iot/constants.py
+++ azext_iot/constants.py
@@ -7,7 +7,7 @@
 
 import os
 
-VERSION = "0.30.0b2.dev3"
+VERSION = "0.30.0b2"
 EXTENSION_NAME = "azure-iot"
 EXTENSION_ROOT = os.path.dirname(os.path.abspath(__file__))
 EXTENSION_CONFIG_ROOT_KEY = "iotext"
